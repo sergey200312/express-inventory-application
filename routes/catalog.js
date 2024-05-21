39e5e7194475265c5ad69c91420462da81365fe0
sergey200312 <sergej-mixajlov-2003@bk.ru>
2024-05-22 01:58:26 +0500

    Создал вид оторбажения для списка авторов

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const gameController = require('../controllers/gameController');
 const genreController = require('../controllers/genreController');
+const authorController = require('../controllers/authorController');
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.get('/', gameController.index);
 router.get('/games', gameController.game_list);
 
 router.get('/genres', genreController.genre_list);
+
+router.get('/authors', authorController.author_list);
 
 module.exports = router;
